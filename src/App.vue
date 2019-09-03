01c6f6272b606ed6718df951149a8778de673341
coastin <coastincoastin@gmail.com>
2019-09-03 15:37:35 +0300

    default vue-app files added

---
+++ src/App.vue
@@ -0,0 +1,396 @@
+<template>
+  <div id="app">
+    <script src="bootstrap.min.js"
+    <header>
+      <div class="container">
+
+<nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="index.html"><img src="./assets/logo-site.png" alt="Logo" class="img-fluid"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse menubar" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Features <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Support</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Blog</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+      </div>
+    </header>
+
+
+
+    <section class="slider">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators c-ind">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner text-left">
+    <div class="carousel-item active">
+      <div class="row">
+        <div class="col-md-5 col-sm-5">
+          <div class="inspiration">
+            <h1>Inspire your inspiration</h1>
+            <h2>Simple to use for your app, products showcase and your inspiration</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tellus tristique bibendum. Donec rutrum sed sem quis venenatis. Proin viverra risus a eros volutpat tempor. In quis arcu et eros porta lobortis sit </p>
+            <ul class="list-inline">
+              <li class="list-inline-item"><i class="fab fa-apple"></i></li>
+              <li class="list-inline-item"><i class="fab fa-android"></i></li>
+              <li class="list-inline-item"><i class="fab fa-windows"></i></li>
+            </ul>
+          </div>
+        </div>
+        <div class="offset-md-2 col-md-5 offset-md-2 col-5 d-md-block d-none">
+          <div class="slide-img text-center">
+            <img src="./assets/mobile.png" alt="mobile" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <div class="carousel-item">
+      <div class="row">
+        <div class="col-md-5 col-sm-5">
+          <div class="inspiration">
+            <h1>Inspire your inspiration</h1>
+            <h2>Simple to use for your app, products showcase and your inspiration</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tellus tristique bibendum. Donec rutrum sed sem quis venenatis. Proin viverra risus a eros volutpat tempor. In quis arcu et eros porta lobortis sit </p>
+            <ul class="list-inline">
+              <li class="list-inline-item"><i class="fab fa-apple"></i></li>
+              <li class="list-inline-item"><i class="fab fa-android"></i></li>
+              <li class="list-inline-item"><i class="fab fa-windows"></i></li>
+            </ul>
+          </div>
+        </div>
+        <div class="offset-md-2 col-md-5 offset-md-2 col-5 d-md-block d-none">
+          <div class="slide-img text-center">
+            <img src="images/mobile.png" alt="mobile" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <div class="carousel-item">
+      <div class="row">
+        <div class="col-md-5 col-sm-5">
+          <div class="inspiration">
+            <h1>Inspire your inspiration</h1>
+            <h2>Simple to use for your app, products showcase and your inspiration</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tellus tristique bibendum. Donec rutrum sed sem quis venenatis. Proin viverra risus a eros volutpat tempor. In quis arcu et eros porta lobortis sit </p>
+            <ul class="list-inline">
+              <li class="list-inline-item"><i class="fab fa-apple"></i></li>
+              <li class="list-inline-item"><i class="fab fa-android"></i></li>
+              <li class="list-inline-item"><i class="fab fa-windows"></i></li>
+            </ul>
+          </div>
+        </div>
+        <div class="offset-md-2 col-md-5 offset-md-2 col-5 d-md-block d-none">
+          <div class="slide-img text-center">
+            <img src="images/mobile.png" alt="mobile" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <section class="service">
+      <div class="container">
+        <div class="row">
+          <div class="feature">
+            <div class="offset-md-1 col-md-10 text-center">
+              <h1 class="title">Tinyone features</h1>
+              <p class="title_content text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tellus tristique bibendum. Donec rutrum sed sem quis venenatis.</p>
+            </div>
+          </div>
+        </div>
+
+
+
+        <div class="row">
+          <div class="col-md-4 col-sm-6 col-12">
+            <div class="single-item">
+              <div class="row">
+                <div class="col-md-3 p-0 text-center">
+                  <div class="single-icon">
+                    <span><i class="fas fa-tablet-alt"></i></span>
+                  </div>
+                </div>
+                <div class="col-md-9  pl-0">
+                  <div class="single_content">
+                    <h1>Fully Responsive</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tellus tristique bibendum.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 col-12">
+            <div class="single-item">
+              <div class="row">
+                <div class="col-md-3 p-0 text-center">
+                  <div class="single-icon">
+                    <span><i class="far fa-lemon"></i></span>
+                  </div>
+                </div>
+                <div class="col-md-9  pl-0">
+                  <div class="single_content">
+                    <h1>Fully Layered PSD</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tellus tristique bibendum.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-12">
+            <div class="single-item">
+              <div class="row">
+                <div class="col-md-3 p-0 text-center">
+                  <div class="single-icon">
+                    <span><i class="far fa-folder"></i></span>
+                  </div>
+                </div>
+                <div class="col-md-9  pl-0">
+                  <div class="single_content">
+                    <h1>Font Awesome Icons</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tellus tristique bibendum.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-12">
+            <div class="single-item">
+              <div class="row">
+                <div class="col-md-3 p-0 text-center">
+                  <div class="single-icon">
+                    <span><i class="fas fa-code"></i></span>
+                  </div>
+                </div>
+                <div class="col-md-9  pl-0">
+                  <div class="single_content">
+                    <h1>HTML3 & CSS3 </h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tellus tristique bibendum.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-12">
+            <div class="single-item">
+              <div class="row">
+                <div class="col-md-3 p-0 text-center">
+                  <div class="single-icon">
+                    <span><i class="far fa-envelope"></i></span>
+                  </div>
+                </div>
+                <div class="col-md-9  pl-0">
+                  <div class="single_content">
+                    <h1>Email Template</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tellus tristique bibendum.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-12">
+            <div class="single-item">
+              <div class="row">
+                <div class="col-md-3 p-0 text-center">
+                  <div class="single-icon">
+                    <span><i class="far fa-bookmark"></i></span>
+                  </div>
+                </div>
+                <div class="col-md-9  pl-0">
+                  <div class="single_content">
+                    <h1>Free to download</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tellus tristique bibendum.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="contact">
+      <div class="container">
+        <div class="row">
+          <div class="contact-heading">
+            <div class="offset-md-1 col-md-10 text-center p-0">
+              <h1 class="title contact_title">Keep in touch with us</h1>
+              <p class="title_content text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tellus tristique bibendum. Donec rutrum sed sem quis venenatis.</p>
+            </div>
+          </div>
+        </div>
+
+        <form>
+          <div class="row">
+            <div class="offset-md-1 col-md-10 offset-sm-1 col-sm-10 col-12">
+              <div class="row">
+                <div class="offset-md-1 col-md-8 offset-sm-1 col-sm-8 col-12 p-0">
+                  <input type="email" class="form-control c-form" placeholder="Enter your email to update">
+                </div>
+                <div class="col-md-2 col-sm-2 col-12 m-0 text-center button">
+                  <button>SUBMIT</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+
+    <section class="all_icon">
+      <div class="container">
+        <div class="row c-padding">
+          <div class="offset-md-4 col-md-4 offset-sm-4 col-sm-4 offset-2 col-8 ">
+            <div class="contact_icon">
+              <div class="row">
+                <div class="col-md-3 col-sm-3 col-3 text-center p-0">
+                  <span><i class="fab fa-facebook"></i></span>
+                </div>
+                <div class="col-md-3 col-sm-3 col-3 text-center p-0">
+                  <span><i class="fab fa-twitter-square"></i></span>
+                </div>
+                <div class="col-md-3 col-sm-3 col-3 text-center p-0">
+                  <span><i class="fab fa-google-plus-square"></i></span>
+                </div>
+                <div class="col-md-3 col-sm-3 col-3 text-center p-0">
+                  <span><i class="fab fa-pinterest-square"></i></span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <div class="container">
+        <div class="row ">          
+          <div class="col-md col-sm-12 col-12">
+            <div class="foot-1">
+              <address>
+                HALOVIETNAM LTD
+                66, Dang Van ngu, Dong Da
+                Hanoi, Vietnam
+                contact@halovietnam.com
+                +844 35149182
+              </address>
+            </div>
+          </div>
+          <div class="col-md  col-sm-6 col-6">
+            <div class="foot">
+              <ul class="list-unstyled">
+                <li><a href="#">Example</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">License</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md col-sm-6 col-6">
+            <div class="foot">
+              <ul class="list-unstyled">
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Privacy</a></li>
+                <li><a href="#">Terms</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md col-sm-6 col-6">
+            <div class="foot">
+              <ul class="list-unstyled">
+                <li><a href="#">Download</a></li>
+                <li><a href="#">Support</a></li>
+                <li><a href="#">Documents</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md col-sm-6 col-6">
+            <div class="foot">
+              <ul class="list-unstyled">
+                <li><a href="#">Media</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Forums</a></li>
+              </ul>
+            </div>
+          </div>          
+        </div>
+      </div> 
+    </footer>
+  </div>
+  
+</template>
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
